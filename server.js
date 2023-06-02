05ed005aa88fbd83586236e9a24b2c27d4849098
avi2420 <93125439+avi2420@users.noreply.github.com>
2023-06-03 00:09:56 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,10 +1,29 @@
-const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
-function datacontrol(req, resp)
+const app = express();
 
-{
-    resp.write("<h1>hello bro</h1>");
-    resp.end();
-}
-http.createServer(datacontrol).listen();
- 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post('/api/form', (req, res) => {
+  const { name, email, message } = req.body;
+
+  console.log('Received form data:');
+  console.log('Name:', name);
+  console.log('Email:', email);
+  console.log('Message:', message);
+
+  const response = {
+    status: 'success',
+    message: 'Form submitted successfully!',
+  };
+
+  res.json(response);
+});
+
+app.listen(5000, () => {
+  console.log('Server is running on http://localhost:5000');
+});
